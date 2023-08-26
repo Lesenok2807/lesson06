715734c562e2262b69201bf439cc118d8605135f
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-26 10:36:58 +0300

    второй коммит к 6 уроку

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("hello!");
+'use strict';
+
+
+function one(num) {
+    function two() {
+        const a = prompt("Угадайте число от 1 до 100");     
+      
+        if (!isNaN(parseFloat(a)) && isFinite(a)) {            
+                
+        if(a >= 1 && a < num) {
+            alert(" Загаданное число больше");
+            two();
+        } else if (a > num && a <= 100) {
+            alert("Загаданное число меньше");
+            two();
+        } else if ( a == num) {
+            alert("Поздравляю, Вы угадали!!!");
+        }                       
+        
+        } else {
+
+             if (a !== null) {
+                 alert("Введите число");
+                 two();
+             } else {
+                alert("Игра окончена");
+             }
+        }          
+        
+    }
+
+    two();
+}  
+
+one(25);
